782beab6b8ee483ef197aef6989a7813844703be
remcopoelstra <remco@poelstrabesturingen.nl>
2023-05-01 17:41:28 +0200

    Initial source commit

---
+++ revenue-ios-core/LineScanner.swift
@@ -0,0 +1,78 @@
+/*
+ * This file is part of Glasspath Revenue.
+ * Copyright (C) 2011 - 2023 Remco Poelstra
+ * Authors: Remco Poelstra
+ *
+ * This program is offered under a commercial and under the AGPL license.
+ * For commercial licensing, contact us at https://glasspath.org. For AGPL licensing, see below.
+ *
+ * AGPL licensing:
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+import Foundation
+
+public class LineScanner: NSObject {
+    
+    var lastString = ""
+    public var stopped = false
+    
+    public func processData(content: Data?) {
+
+        if lineReceived != nil {
+
+            if let data = content, !data.isEmpty {
+                
+                let dataAsString = String(data: data, encoding: .utf8)
+                if dataAsString != nil {
+
+                    let s = lastString + dataAsString!
+                    // print("LineScanner received: \(s)")
+
+                    let lines = s.split(whereSeparator: \.isNewline)
+                    if lines.count > 0 {
+
+                        var to = lines.count
+
+                        // If received string doesn't end with newLine then we need to store the last part
+                        let lastNewLineIndex = s.lastIndex(where: \.isNewline)?.utf16Offset(in: s) ?? -1
+                        // print("s: \(s), lastNewLineIndex: \(lastNewLineIndex)")
+                        if lastNewLineIndex < s.count - 1 {
+                            to -= 1
+                            lastString = String(lines[lines.count - 1])
+                        } else {
+                            lastString = ""
+                        }
+                        
+                        for i in 0..<to {
+                            lineReceived!(String(lines[i]))
+                        }
+                            
+                    }
+
+                }
+                
+            }
+
+        }
+
+    }
+    
+    func reset() {
+        lastString = ""
+        stopped = false
+    }
+    
+    final public var lineReceived: ((_ line: String) -> Void)?
+    
+}
---
+++ revenue-ios-core/revenue_ios_core.h
@@ -0,0 +1,18 @@
+//
+//  revenue_ios_core.h
+//  revenue-ios-core
+//
+//  Created by Remco Poelstra on 29/04/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for revenue_ios_core.
+FOUNDATION_EXPORT double revenue_ios_coreVersionNumber;
+
+//! Project version string for revenue_ios_core.
+FOUNDATION_EXPORT const unsigned char revenue_ios_coreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <revenue_ios_core/PublicHeader.h>
+
+
